0ce65a668f0461113b67409415a07d17b0c07348
José Saantos <contato.josesantosdev@gmail.com>
2024-06-20 23:51:36 -0300

    Args
    aula 6

--- Passando argumentos para o programa/Argumentos.java
+++ Passando argumentos para o programa/Argumentos.java
@@ -1,6 +1,6 @@
 class Argumentos {
     public static void main (String[] args){
         //podemos acessar o vetor de strings passado na função main
-        System.out.println("Você digitou" + args[0]);
+        System.out.println("Você digitou " + args[0]);
     }
 }
